63ac0ff007863c3eb0998e11299549b24cc3380d
nitsujader <justinreda@gmail.com>
2014-10-19 05:37:20 -0400

    adding randomizer & layout effects

--- app/src/main/java/com/justinreda/jredaprogrammingassignment/FirstActivity.java
+++ app/src/main/java/com/justinreda/jredaprogrammingassignment/FirstActivity.java
@@ -1,27 +1,240 @@
 package com.justinreda.jredaprogrammingassignment;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+/**
+ * Created by Justin on 10/19/2014.
+ */
 public class FirstActivity extends Activity {
+
+    String FIELD_ID = "id";
+    String FIELD_NAME = "name";
+    String FIELD_DESCRIPTION = "description";
+    String FIELD_REGULAR_PRICE = "regular_price";
+    String FIELD_SALE_PRICE = "sale_price";
+    String FIELD_PRODUCT_IMAGE = "product_image";
+    String FIELD_COLORS = "colors";
+    String FIELD_STORES = "stores";
+    String FIELD_STORES_NUMBER = "store_number";
+    String FIELD_STORES_STOCK = "store_stock";
+
+    String MOCK_DATA_FILENAME = "ExampleData.json";
+
+    Button createProductButton;
+    Button showProductButton;
+
+    TextView addProductOptionsTV;
+
+    LinearLayout initialButtonsRowLL;
+    LinearLayout createProductMethodLL;
+    LinearLayout addProductMethodOptionsLL;
+    LinearLayout fromFileOptionsLL;
+    LinearLayout internalSystemOptionsLL;
+    LinearLayout enterManuallyLL;
+    LinearLayout fromSQLLL;
+    LinearLayout[] importOptionsLLs;
+
+    RadioGroup createProductsMethodsRG;
+
+    LinearLayout showProductLL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        setupInitialButtonListeners();
     }
 
+    /**
+     * Initializes the UI elements, initializes variables, adds listeners
+     */
+    private void setupInitialButtonListeners() {
+        createProductButton = (Button) findViewById(R.id.add_product_BUT);
+        showProductButton = (Button) findViewById(R.id.show_product_BUT);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return true;
+        addProductOptionsTV = (TextView) findViewById(R.id.add_product_method_options_title_TV);
+
+        initialButtonsRowLL = (LinearLayout) findViewById(R.id.initial_buttons_row_LL);
+        createProductMethodLL = (LinearLayout) findViewById(R.id.create_product_method_LL);
+        addProductMethodOptionsLL = (LinearLayout) findViewById(R.id.add_product_method_options_LL);
+        importOptionsLLs = new LinearLayout[2];
+        fromFileOptionsLL = (LinearLayout) findViewById(R.id.from_file_options_LL);
+        importOptionsLLs[0] = fromFileOptionsLL;
+        internalSystemOptionsLL = (LinearLayout) findViewById(R.id.internal_randomizer_LL);
+        importOptionsLLs[1] = internalSystemOptionsLL;
+       /* enterManuallyLL = (LinearLayout)findViewById(R.id.initial_buttons_row_LL);//TODO
+        importOptionsLLs[2]=enterManuallyLL;
+        fromSQLLL = (LinearLayout)findViewById(R.id.initial_buttons_row_LL);//TODO
+        importOptionsLLs[3]=fromSQLLL;*/
+
+        createProductsMethodsRG = (RadioGroup) findViewById(R.id.add_product_method_RG);
+        createProductsMethodsRG.clearCheck();
+        createProductsMethodsRG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                //Hide all the other views
+                for (LinearLayout layout : importOptionsLLs) {
+                    if (layout.getVisibility() == View.VISIBLE) {
+                        layout.setVisibility(View.GONE);
+                    }
+                }
+
+                switch (checkedId) {
+                    case R.id.apm_from_file_RB: // from file
+                        //TODO: change the title of the section
+                        fromFileOptionsLL.setVisibility(View.VISIBLE);
+
+                        break;
+                    case R.id.apm_internal_system_RB: // internal randomizer
+                        addProductOptionsTV.setText("Please enter values below");
+                        internalSystemOptionsLL.setVisibility(View.VISIBLE);
+
+                        final EditText numEntriesET = (EditText) findViewById(R.id.randomizer_entries_ET);
+                        final EditText numStoresET = (EditText) findViewById(R.id.randomizer_stores_ET);
+                        final Button runBUT = (Button) findViewById(R.id.run_randomizer_BUT);
+                        runBUT.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Utilities.hideKeyboard(getApplicationContext(), runBUT);
+
+                                int entries = 3;
+                                if (Utilities.etNotNullNotEmpty(numEntriesET)) {
+                                    entries = Integer.parseInt(numEntriesET.getText().toString());
+                                } else {
+                                    numEntriesET.setText("" + entries);
+                                }
+
+                                int stores = 3;
+                                if (Utilities.etNotNullNotEmpty(numStoresET)) {
+                                    stores = Integer.parseInt(numStoresET.getText().toString());
+                                } else {
+                                    numStoresET.setText("" + stores);
+                                }
+
+                                runRandomizer(entries, stores);
+                                Toast.makeText(getApplicationContext(), "Done!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                        break;
+                   /* case 2: // enter manually
+                        break;
+                    case 3: // saved sql db
+                        break;*/
+                }
+            }
+        });
+
+
+        createProductButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //real functionality
+                if (showProductLL.getVisibility() == View.VISIBLE) {
+                    showProductLL.setVisibility(View.GONE);
+                }
+                createProductMethodLL.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+
+        showProductButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                runRandomizer(3, 4);
+            }
+        });
+
+        showProductLL = (LinearLayout) findViewById(R.id.show_product_LL);
+
+        /*Dictionary<String, Integer> storesDictionary;
+        storesDictionary = new Hashtable<String, Integer>();
+        storesDictionary.*/
+    }
+
+    private void runRandomizer(int numEntries, int numStores) {
+        try {
+
+            double priceMin = 15.00;
+            double priceMax = 99.99;
+            int stockMin = 0;
+            int stockMax = 15;
+
+            JSONArray itemsArray = new JSONArray();
+            for (int i = 0; i < numEntries; i++) {
+                JSONObject item = new JSONObject();
+
+                item.put(FIELD_ID, i);
+                item.put(FIELD_NAME, "Item " + i);
+                item.put(FIELD_DESCRIPTION, "Description " + i);
+
+                double randomRegular = Utilities.getRandomDoubleInRange(priceMin, priceMax);
+                randomRegular = Utilities.truncateDouble(randomRegular, 2);
+                double randomSale = randomRegular * 0.75;
+                randomSale = Utilities.truncateDouble(randomSale, 2);
+                item.put(FIELD_REGULAR_PRICE, randomRegular);
+                item.put(FIELD_SALE_PRICE, randomSale);
+
+                item.put(FIELD_PRODUCT_IMAGE, "Image Name " + i);
+
+                JSONArray colorsArray = new JSONArray();
+                colorsArray.put("Red");
+                colorsArray.put("Blue");
+                colorsArray.put("Green");
+                colorsArray.put("Black");
+                colorsArray.put("Yellow");
+                colorsArray.put("Orange");
+                if (i % 2 == 0) colorsArray.put("Gold");
+                if (i % 2 == 0) colorsArray.put("Silver");
+                if (i % 2 == 0) colorsArray.put("Chrome");
+                item.put(FIELD_COLORS, colorsArray);
+
+                JSONArray storesArray = new JSONArray();
+                for (int j = 0; j < numStores; j++) {
+                    JSONObject store = new JSONObject();
+                    store.put(FIELD_STORES_NUMBER, "Store " + j);
+                    int randomStock = Utilities.getRandomIntInRange(stockMin, stockMax);
+                    store.put(FIELD_STORES_STOCK, randomStock);
+                    storesArray.put(store);
+                }
+                item.put(FIELD_STORES, storesArray);
+
+                itemsArray.put(item);
+            }
+
+            Utilities.writeDownloadsSTRFile(MOCK_DATA_FILENAME, itemsArray.toString());
+            showProductButton.setEnabled(true);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+    protected void onSaveInstanceState(Bundle outgoingState) {
+        super.onSaveInstanceState(outgoingState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedState) {
+        super.onRestoreInstanceState(savedState);
     }
 }
---
+++ app/src/main/java/com/justinreda/jredaprogrammingassignment/Utilities.java
@@ -0,0 +1,270 @@
+package com.justinreda.jredaprogrammingassignment;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.util.Random;
+
+/**
+ * Utility class containing commonly used methods by Justin Reda
+ * <p/>
+ * The contained methods are general purpose, and not specific to any
+ * application so they should work with any version of API.
+ */
+@SuppressWarnings("unused")
+public class Utilities {
+
+    static String TAG = "Utilities";
+
+    /**
+     * Write a text file to the downloads directory, file name and string data provided at use.
+     *
+     * @param filename String of the filename to write, Ex) "file.txt"
+     * @param data     String of the physical data to put into the new file.
+     * @return True if file written successfully
+     */
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public static boolean writeDownloadsSTRFile(String filename, String data) {
+
+        Log.d(TAG, "Writing file [" + filename + "] to downloads directory");
+        File[] downloadFiles = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).listFiles();
+
+        for (File downloadFile : downloadFiles) {
+            if (downloadFile.getName().matches(filename)) {
+                downloadFile.delete();
+            }
+        }
+
+        File myFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/" + filename);
+        try {
+            myFile.createNewFile();
+            FileOutputStream fOut = new FileOutputStream(myFile);
+            OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
+            myOutWriter.append(data);
+            myOutWriter.close();
+            fOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * This method writes data to the internal storage directory of the app given appropriate input parameters, filename and data.
+     *
+     * @param filename String of the filename to write, Ex) "file.txt"
+     * @param value    String of the physical data to put into the new file.
+     * @return True if file written successfully
+     */
+    public static boolean writeInternalStorageSTRFile(Context context, String filename, String value) {
+
+        try {
+            FileOutputStream fos = context.openFileOutput(filename, Context.MODE_PRIVATE);
+            fos.write(value.getBytes());
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d(TAG, "Problem writing file [" + filename + "] to internal storage");
+            return false;
+        }
+
+        Log.d(TAG, "File [" + filename + "]added = " + checkForFile(context, filename));
+        return true;
+    }
+
+    /**
+     * This will return a file from the internal storage directory of the app.
+     *
+     * @param filename String of the filename to be retrieved
+     * @return a long string of the data from the retrieved file, empty string if problem.
+     */
+    public static String returnInternalStorageSTRFile(Context context, String filename) {
+
+        FileInputStream fis;
+        String data;
+        if (checkForFile(context, filename)) {
+            try {
+                fis = context.openFileInput(filename);
+                InputStreamReader in = new InputStreamReader(fis);
+                BufferedReader br = new BufferedReader(in);
+                data = br.readLine();
+                fis.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "";
+            }
+        } else {
+            return "";
+        }
+        return data;
+    }
+
+    /**
+     * This method will check to see if a file exists, given its full file path
+     *
+     * @param fileName String of the path to file and name of file to check for.
+     * @return boolean, true if file exists, false otherwise
+     */
+    public static boolean checkForFile(Context context, String fileName) {
+        File file = context.getFileStreamPath(fileName);
+        return file.exists();
+    }
+
+    /**
+     * Delete a file stored in internal private app storage
+     *
+     * @param context  application context
+     * @param filename string of filename to be deleted
+     * @return true if successfully deleted
+     */
+    public static boolean deleteInternalFile(Context context, String filename) {
+        Log.d(TAG, "Deleting internal file " + filename);
+        File file = context.getFileStreamPath(filename);
+        return file.delete();
+    }
+
+    /**
+     * String value helper method
+     *
+     * @param string value to be inspected
+     * @return true if not null, length greater then 0, and found text
+     */
+    public static boolean strNotNullNotEmpty(String string) {
+        return string != null && string.length() > 0 && !string.matches("");
+    }
+
+    /**
+     * EditText helper method
+     *
+     * @param et value to be inspected
+     * @return true if not null, length greater then 0, and found text
+     */
+    public static boolean etNotNullNotEmpty(EditText et) {
+        return et != null && et.getText() != null && et.getText().toString().length() > 0 && !et.getText().toString().matches("");
+    }
+
+    /**
+     * Returns the screen density of the device
+     * xxhdpi: 3.0
+     * xhdpi: 2.0
+     * hdpi: 1.5
+     * mdpi: 1.0 (baseline)
+     * ldpi: 0.75
+     *
+     * @param context application context
+     * @return float value of screen density
+     */
+    public static float getScreenDensity(Context context) {
+        return context.getResources().getDisplayMetrics().density;
+    }
+
+    /**
+     * Fetches and returns file contents as a string given a url
+     *
+     * @param url String path to remote file location
+     * @return String contents of file at remote url
+     */
+    public static String getStringFromURL(String url) {
+
+        String stringText = "";
+
+        try {
+            URL textUrl = new URL(url);
+            BufferedReader bufferReader = new BufferedReader(new InputStreamReader(textUrl.openStream()));
+            String StringBuffer;
+            while ((StringBuffer = bufferReader.readLine()) != null) {
+                stringText += StringBuffer;
+            }
+            bufferReader.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringText;
+    }
+
+    public static int getRandomIntInRange(int min, int max) {
+        Random random = new Random();
+        return random.nextInt((max - min) + 1) + min;
+    }
+
+    public static double getRandomDoubleInRange(double min, double max) {
+        Random random = new Random();
+        return (min + (max - min) * random.nextDouble());
+    }
+
+    public static double truncateDouble(double inputNumber, int places) {
+        String pattern = "#.";
+        for (int i = 0; i < places; i++) {
+            pattern += "#";
+        }
+        DecimalFormat twoDForm = new DecimalFormat(pattern);
+        return Double.valueOf(twoDForm.format(inputNumber));
+    }
+
+    public static float getRandomFloatInRange(float min, float max) {
+        Random random = new Random();
+        return (min + (max - min) * random.nextFloat());
+    }
+
+    public static void hideKeyboard(Context context, View view) {
+        if (context != null && view != null) {
+            InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    /**
+     * This will read a file from the downloads directory and return the content of file as a string.
+     *
+     * @param filename file which you want to be retrieved. A string is expected. Ex) "file.txt"
+     * @return the contents of the file as a String
+     */
+    private String readDownloadSTRFile(String filename) {
+        File[] downloadFiles = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).listFiles();
+        File goodFile = null;
+
+        for (File downloadFile : downloadFiles) {
+            if (downloadFile.getName().matches(filename)) {
+                goodFile = downloadFile;
+            }
+        }
+        if (goodFile == null) {
+            return "";
+        }
+
+        StringBuilder text = new StringBuilder();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(goodFile));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text.toString();
+    }
+
+}
